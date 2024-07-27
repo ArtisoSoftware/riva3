757c0cb6cef0ffbfb3fcc304312eeebca98f9366
ArtisoSoftware <artisosoftware@gmail.com>
2024-07-27 23:51:07 +0700

    build _datatab

--- src/components/ri_buildyourown/Datatab_dimension.vue
+++ src/components/ri_buildyourown/Datatab_dimension.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>test</div>
+  <div class="bgGrey font-16 q-pt-md" align="center">
+    <div style="width: 90%; margin: auto">
+      <div class="row items-center">
+        <div class="font-12" align="left">
+          <div class="font-30"><b>Dimension</b></div>
+          <div class="">Select a dimension to see detail</div>
+        </div>
+        <div style="width: 450px">
+          <q-select
+            class="inputSelectClass"
+            dark
+            dense
+            v-model="selected"
+            :style="{ background: colorSelected }"
+            :options="dimensionOptions"
+            @update:model-value="changeDimension()"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -55,10 +75,15 @@ const yourGroupName = ref("Your group");
 const dimPick = ref([]);
 const firstHalfPeriod = ref("");
 const secondHalfPeriod = ref("");
-const countAllPair = 0;
+const countAllPair = ref(0);
 const report = ref([]);
 const partner = ref([]);
-const selected = ref("");
+const selected = ref(0);
+const colorSelected = ref("");
+const dimensionIndex = ref(0);
+
+const indicatorStr = ref([]);
+
 onMounted(() => {
   checkYourName();
 });
@@ -77,7 +102,7 @@ const loadData = async () => {
   dimPick.value = props.input.dimensionPicked;
   if (props.input.year.min == props.input.year.max - 1) {
     firstHalfPeriod.value = props.input.year.min;
-    secondHlafPeriod.value = props.input.year.max;
+    secondHalfPeriod.value = props.input.year.max;
   } else {
     let diffYear = Math.floor(
       (props.input.year.max - props.input.year.min) / 2
@@ -92,19 +117,45 @@ const loadData = async () => {
   });
   countAllPair.value = 0;
   report.value.forEach((report) => {
-    partner.forEach((partner) => {
+    partner.value.forEach((partner) => {
       if (report != partner) {
         countAllPair.value++;
       }
     });
   });
+
   selected.value = dimensionOptions.value[0];
   changeDimension();
 };
 
 const changeDimension = async () => {
-  console.log("working here");
+  dimensionIndex.value = dimensionName.value.indexOf(selected.value);
+  colorSelected.value = dimensionColor.value[dimensionIndex.value];
+  dimensionIndex.value++;
+  loadAllChart();
+};
+
+const loadAllChart = () => {
+  setIndexChart();
+  console.log("work here");
+};
+
+const setIndexChart = async () => {
+  indicatorStr.value =
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.bgGrey {
+  background: #ededed;
+  height: 950px;
+}
+.inputSelectClass {
+  background: #2d9687;
+  padding-left: 14px;
+  width: 400px;
+  color: white;
+  height: 40px;
+  font-size: 24px;
+}
+</style>
